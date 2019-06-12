f93cb2ee63db6328fdde918da0032976d23b0baf
DIOGO F B RODRIGUES <diogo.borbar@gmail.com>
2019-06-12 13:11:00 -0300

    atualização todas as médias
    Atualização final para tirar média de todas ás variaveis

--- TowerFluxData.vb
+++ TowerFluxData.vb
@@ -1,49 +1,47 @@
 Sub MediaDiaNoite()
-    linha = 2 'ALTERAR PARA NOVA ANALISE
-    MediaTemp = 0
-    divisor = 0
-    linhanova = 2
-    linhaNUll = 0
-    colunaDaVariavel = 25 'ALTERAR PARA NOVA ANALISE
-    ColunaAno = 1 'ALTERAR PARA NOVA ANALISE
-    ColunaDia = 2 'ALTERAR PARA NOVA ANALISE
-    Aba = "N_AVG_day" 'ALTERAR PARA NOVA ANALISE
-    
-	
+colLege = 5
+linLege = 6
+ColunaVariavel = 5 'alterar para nova variavel
+Aba = "N_AVG_day" 'ALTERAR PARA NOVA ANALISE
+colunaNova = 3 'ALTERAR PARA NOVA ANALISE
+    Do While Not IsEmpty(Cells(linLege, colLege))
+      linha = 6
+      MediaTemp = 0
+      divisor = 0
+      linhanova = 2
+      linhaNUll = 0
     Do While Not IsEmpty(Range("A" & linha))
       
        If IsEmpty(Range("A" & linha)) And IsEmpty(Range("A" & linha + 1)) Then
             Exit Do
             
         Else
-            If Cells(linha, ColunaDia) = Cells(linha + 1, ColunaDia) Then
+            If Cells(linha, 2) = Cells(linha + 1, 2) Then
                 
-                If Cells(linha, colunaDaVariavel) = "Null" Or Cells(linha, colunaDaVariavel) < 0 Then
+                If Cells(linha, ColunaVariavel) = "Null" Then
                   
                 Else
-                    MediaTemp = MediaTemp + Cells(linha, colunaDaVariavel)
+                    MediaTemp = MediaTemp + Cells(linha, ColunaVariavel)
                     divisor = divisor + 1
                 End If
                 
             Else
-                If MediaTemp <= 0 Then 'ALTERADO NA CHUVA
-                      'ThisWorkbook.Sheets(Aba).Range("A" & linhanova).Value = Cells(linha, ColunaAno)
-                      'ThisWorkbook.Sheets(Aba).Range("B" & linhanova).Value = Cells(linha, ColunaDia)
-                      ThisWorkbook.Sheets(Aba).Range("X" & linhanova).Value = "Null"      'ALTERAR PARA NOVA ANALISE
+                If MediaTemp = 0 Then
+                      ThisWorkbook.Sheets(Aba).Range("A" & linhanova).Value = Cells(linha, 1)
+                      ThisWorkbook.Sheets(Aba).Range("B" & linhanova).Value = Cells(linha, 2)
+                      ThisWorkbook.Sheets(Aba).Cells(linhanova, colunaNova) = "Null" 'Alterar para nova analise
                       divisor = 0
                       MediaTemp = 0
                       linhanova = linhanova + 1
                     
                 Else
-                    
-                    If Cells(linha, colunaDaVariavel) = "Null" Or Cells(linha, colunaDaVariavel) <= 0 Then 'ALTERADO NA CHUVA
-                       Call preecherTab(linhanova, linha, MediaTemp, divisor, ColunaAno, ColunaDia, Aba)
+                    If Cells(linha, ColunaVariavel) = "Null" Then
+                       Call preecherTab(linhanova, linha, MediaTemp, divisor, Aba, colunaNova)
                        
                     Else
-                        
-                        MediaTemp = MediaTemp + Cells(linha, colunaDaVariavel)
+                        MediaTemp = MediaTemp + Cells(linha, ColunaVariavel)
                         divisor = divisor + 1
-                        Call preecherTab(linhanova, linha, MediaTemp, divisor, ColunaAno, ColunaDia, Aba)
+                        Call preecherTab(linhanova, linha, MediaTemp, divisor, Aba, colunaNova)
                        
                     End If
                                     
@@ -56,15 +54,21 @@ Sub MediaDiaNoite()
         End If
 
     Loop
-
+    colunaNova = colunaNova + 1
+    ColunaVariavel = ColunaVariavel + 1
+    colLege = colLege + 1
+Loop
 End Sub
 
-Sub preecherTab(linhanova, linha, MediaTemp, divisor, ColunaAno, ColunaDia, Aba)
+Sub preecherTab(linhanova, linha, MediaTemp, divisor, Aba, colunaNova)
     MediaDiaria = MediaTemp / divisor
-    'ThisWorkbook.Sheets(Aba).Range("A" & linhanova).Value = Cells(linha, ColunaAno)
-    'ThisWorkbook.Sheets(Aba).Range("B" & linhanova).Value = Cells(linha, ColunaDia)
-    ThisWorkbook.Sheets(Aba).Range("X" & linhanova).Value = MediaDiaria     'ALTERAR PARA NOVA ANALISE
+    ThisWorkbook.Sheets(Aba).Range("A" & linhanova).Value = Cells(linha, 1)
+    ThisWorkbook.Sheets(Aba).Range("B" & linhanova).Value = Cells(linha, 2)
+    ThisWorkbook.Sheets(Aba).Cells(linhanova, colunaNova) = MediaDiaria 'Alterar para nova analise
     divisor = 0
     MediaTemp = 0
     linhanova = linhanova + 1
 End Sub
+
+
+
